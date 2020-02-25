1e64bc893bf71821c2e42272d15dac755cf87780
Leo <leo.zhou1986@hotmail.com>
2020-02-25 23:41:58 +1300

    Add server url

--- WebApp/ClientApp/src/env/Env.js
+++ WebApp/ClientApp/src/env/Env.js
@@ -1,4 +1,5 @@
 export const serverUrl = "http://192.168.1.2:60601/";
+//export const serverUrl = "https://signalhubchat.azurewebsites.net/";
 
 export const MENU = [
   { key: 1, text: "Chats", value: "Chats", icon: "chat" },
